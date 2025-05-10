d6539beaa2faa8b5b3f324d9583af42ef29e27bb
Azeemullah Ansari <ansariazeemullah786@gmail.com>
2025-05-10 20:59:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function CheckLogin() {
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+    document.getElementById('message').innerText = "Checking Authentication...";
+
+    setTimeout(() => {
+        if (username.toLowerCase() == 'admin') {
+            if (password.toLowerCase() == 'azeemullah') {
+                document.getElementById('message').innerText = "Successfull Loged in!"
+                setTimeout(() => {
+                    window.location.href = "index.html"
+                }, 1000);
+            }
+            else {
+                document.getElementById('message').innerText = "Wrong Input."
+            }
+        }
+        else {
+            document.getElementById('message').innerText = "Failed Try Again..."
+        }
+    }, 2000);
+}
+
